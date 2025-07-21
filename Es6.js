339370f86e6b41a79aae5a4424fa305adffb95b1
mydul <mydul11info@gmail.com>
2025-07-22 02:29:25 +0600

    add the templete string

--- Es6.js
+++ Es6.js
@@ -6,3 +6,13 @@ const maltiple = (a=0 , b=0 , c=0) => a * b * c;
 
 const malti = maltiple( 10, 5, 5 );
 console.log(malti);
+
+
+// 2. templete string
+
+const myself = `
+    I am a Web developer.
+    I love to code.
+    I love to eat Briyani.
+`;
+console.log(myself);

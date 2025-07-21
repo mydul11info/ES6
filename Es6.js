03b4776d586926f99edcfb6f57b633bcff5ca1aa
mydul <mydul11info@gmail.com>
2025-07-22 02:42:40 +0600

    added the defult parameter

--- Es6.js
+++ Es6.js
@@ -16,3 +16,11 @@ const myself = `
     I love to eat Briyani.
 `;
 console.log(myself);
+
+
+// 3. defult parameter
+function add(num1=0, num2=0) {
+    const result = num1 + num2;
+    return result
+};
+console.log(add(20, 30));

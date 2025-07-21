20e2076b4a6e6734705769e135300ec3b20856e4
mydul <mydul11info@gmail.com>
2025-07-22 02:35:46 +0600

    carection the arow functuon

--- Es6.js
+++ Es6.js
@@ -2,9 +2,9 @@
 
 // 1. arow function
 
-const maltiple = (a=0 , b=0 , c=0) => a * b * c;
+const maltiple = (a, b, c) => a * b * c;
 
-const malti = maltiple( 10, 5, 5 );
+const malti = maltiple( 10, 5,  5);
 console.log(malti);
 
 
